a7da4aaa0b9c7479bbeeee85b97c4c4d66b025bf
Zhang Mao <616749153@qq.com>
2020-06-23 17:30:05 +0800

    框架初始化

---
+++ README.md
@@ -0,0 +1,25 @@
+<!-- 基础架构 集成了 iview+echarts+vue-quill-editor 按需引入-->
+# iview-demo
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <h1 class="tips">Welcome to the Information Center</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// You can also register Quill modules in the component
+  
+  export default {
+  }
+</script>
+<style lang="less">
+.tips{
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-top: -80px;
+  margin-left: -230px;
+}
+</style>
